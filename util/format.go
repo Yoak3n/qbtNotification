c62ffebb47427d848cb5225b0abe1cc8a199c5f7
Yoake <3354590676@qq.com>
2023-06-11 18:06:25 +0800

    fix: message of download completely format error

--- util/format.go
+++ util/format.go
@@ -9,7 +9,7 @@ func FormatMsg(flag bool, text *string) (msg string) {
 	if flag {
 		msg = fmt.Sprintf("\t\tqBittorrent已开始下载：\n%s", *text)
 	} else {
-		msg = fmt.Sprintf("\t\tqBittorrent已完成下载：\n\n%s", *text)
+		msg = fmt.Sprintf("\t\tqBittorrent已完成下载：\n%s", *text)
 	}
 	msg = url.QueryEscape(msg)
 	return
